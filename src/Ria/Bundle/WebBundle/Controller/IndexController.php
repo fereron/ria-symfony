521043da1d68b922a02e831a2f1b844a25daf525
vagifrzada <vaqifrzade@gmail.com>
2020-10-28 16:29:55 +0400

    Refactored Controllers and routes.yml

--- src/Ria/Bundle/WebBundle/Controller/IndexController.php
+++ src/Ria/Bundle/WebBundle/Controller/IndexController.php
@@ -3,18 +3,21 @@ declare(strict_types=1);
 
 namespace Ria\Bundle\WebBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * Class IndexController
+ * @package Ria\Bundle\WebBundle\Controller
+ */
 class IndexController extends AbstractController
 {
-
     /**
      * @Route("/", name="index")
      */
     public function index()
     {
-        dd('This is main frontend page');
+        return new Response("<h1>Welcome to the homepage !</h1>");
     }
-
 }
